186a58588d328a4cdf19e2b285f8bfa300f02d81
DianaLease <DianaRLease@gmail.com>
2018-02-02 15:51:45 -0500

    updated models

--- server/db/models/item.js
+++ server/db/models/item.js
@@ -2,28 +2,18 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Item = db.define('item', {
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    unique: true,
-    validate: {
-      notEmpty: true
+  latitude: {
+    type: Sequelize.DOUBLE
+  },
+  longitude: {
+    type: Sequelize.DOUBLE
+  },
+  hidden: {
+    type: Sequelize.VIRTUAL,
+    get () {
+      if (this.getDataValue('latitude')) return true
+      else return false
     }
-  },
-  points: {
-    type: Sequelize.INTEGER,
-    validate: {
-      max: 100,
-      min: 0
-    }
-  },
-  modelUrl: {
-    type: Sequelize.STRING
-    //not sure what data will be stored here yet
-  },
-  emoji: {
-    type: Sequelize.STRING,
-    allowNull: false
   }
 })
 
--- server/db/models/item.spec.js
+++ server/db/models/item.spec.js
@@ -1,6 +1,6 @@
 const { expect } = require('chai')
 const db = require('../index')
-const { User, Item, UserItem } = require('./index')
+const { User, Item, ItemCategory } = require('./index')
 
 //this doesn't work
 
@@ -16,17 +16,17 @@ describe('User Item join table', () => {
     beforeEach(async () => {
 
       const items = await Promise.all([
-        Item.create({
+        ItemCategory.create({
           name: 'Peach',
           points: 1,
           threeJS: 'https://www.cgtrader.com/free-3d-models/food/fruit/apricot-lowpoly'
         }),
-        Item.create({
+        ItemCategory.create({
           name: 'Apple',
           points: 14,
           threeJS: 'https://www.cgtrader.com/free-3d-print-models/art/scans-replicas/red-apple--3'
         }),
-        Item.create({
+        ItemCategory.create({
           name: 'Strawberry',
           points: 98,
           threeJS: 'https://www.cgtrader.com/free-3d-models/food/fruit/strawberry-e58fc22b7ea3c5bc232d0c2229c6971c'
@@ -52,7 +52,7 @@ describe('User Item join table', () => {
       ])
 
       await Promise.all([
-        UserItem.update({
+        Item.update({
           longitude: 40.704761,
           latitude: -74.009133
         },
@@ -61,7 +61,7 @@ describe('User Item join table', () => {
               id: 1
             }
           }),
-        UserItem.update({
+        Item.update({
           longitude: 40.704966,
           latitude: -74.009491
         },
@@ -73,12 +73,12 @@ describe('User Item join table', () => {
       ])
 
 
-      await UserItem.findById(1)
+      await Item.findById(1)
         .then((item) => {
           userItem1 = item;
         })
 
-      await UserItem.findById(2)
+      await Item.findById(2)
         .then((item) => {
           userItem2 = item;
         })
