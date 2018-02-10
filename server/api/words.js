c906b1fcdb7805e2dc884a73412dc7545d47cbe6
Eetai Ben-Sasson <eeetai@gmail.com>
2018-02-10 11:15:58 -0500

    delete route added

--- server/api/words.js
+++ server/api/words.js
@@ -7,7 +7,7 @@ router.get('/', (req, res, next) => {
   Word.findAll()
     .then(words => res.json(words))
     .catch(next)
-  }
+}
 )
 
 router.post('/', (req, res, next) => {
@@ -15,3 +15,24 @@ router.post('/', (req, res, next) => {
     .then(word => res.json(word))
     .catch(next)
 })
+
+router.put('/', (req, res, next) => {
+  Word.update(req.body, {
+    where: {
+      id: req.params.userId
+    }
+  })
+    .then(word => res.json(word))
+    .catch(next)
+})
+
+router.delete('/:wordId', (req, res, next) => {
+  Word.destroy({
+    where: {
+      id: req.params.wordId
+    }
+  })
+    .then(() => res.status(202).send('Deleted'))
+    .catch(next)
+})
+
